6bce8501bc6f971d021f9893ec4207320ddf0f15
GeoMaster101 <GiovanniCagliari73@gmail.com>
2024-05-09 18:57:27 -0700

    adds local Storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,32 @@
+  /** DATA FIELDS **/
 const SearchInput = document.querySelector('#SearchInput');
 const search = document.querySelector('#search-button');
 
 search.addEventListener('click', function (event) {
   event.preventDefault();
-
+  
+  let user = JSON.parse(localStorage.getItem('user'));
+  if (!user) 
+  {
+    user = {
+      search: [ ]
+    };
+    }
   // create user object from submission
-  const user = {
-    Search: SearchInput.value.trim(),
-  };
-
+    user.search.push(SearchInput.value.trim())
+    
   // set new submission to local storage
   localStorage.setItem('user', JSON.stringify(user));
 });
+
+// Retrieve Searchs and SearchId from localStorage
+let PreviousSearchs = JSON.parse(localStorage.getItem("user"));
+if (PreviousSearchs === null) {
+  PreviousSearchs = [];
+}
+
+let SearchId = JSON.parse(localStorage.getItem("SearchId"))
+  ? JSON.parse(localStorage.getItem("SearchId"))
+  : [];
+
 
