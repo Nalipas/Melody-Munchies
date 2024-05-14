683a9fd42df01c1a955b4df1982cbe7a754f04ab
Nalipas <smoreno2014@gmail.com>
2024-05-13 18:54:26 -0700

    fixes spelling errors, adjusts styling, fixes styling bug with display of cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,7 @@
   console.log(mockData);
 const searchInput = document.querySelector('#searchInput');
 const search = document.querySelector('#search-button');
-
-
-
-// $.ajax(spotifySearch).done(function (response) {
-// 	console.log(response);
-// }); 
-
-const trackData = {
-	async: true,
-	crossDomain: true,
-	url: 'https://spotify-scraper.p.rapidapi.com/v1/track/metadata?trackId=5ubHAQtKuFfiG4FXfLP804',
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '8b0f6327b4msh023252b9681cd48p15ed04jsn5cb8133c35a7',
-		'X-RapidAPI-Host': 'spotify-scraper.p.rapidapi.com'
-	}
-};
+const previousSearches = document.querySelector('#previousSearches');
 
 // $.ajax(trackData).done(function (response) {
 //	console.log(response);
@@ -81,7 +65,7 @@ displaySearchResults(mockData);
 function displaySearchResults(data) {
   $('#searchResults').empty();
 
-  const results = $('<div id="results">');
+  const results = $('<div class="columns" id="results">');
 
   for (let i = 0; i <= 4; i++) {
     const track = data.tracks.items[i];
@@ -90,13 +74,12 @@ function displaySearchResults(data) {
     const trackName = track.name;
     const artist = track.artists[0].name;
     const albumName = album.name;
-    const releaseDate = album.release_date;
     const trackId = track.id;
     const trackUrl = `https://open.spotify.com/track/${trackId}`;
 
     const trackCard = $(`
       <div class="card column">
-        <img src="${albumArt}" class="card-img-top is-small" alt="Album Art">
+        <img src="${albumArt}" class="column card-img-top is-small" alt="Album Art">
         <div class="card-body">
           <h5 class="card-title">${trackName}</h5>
           <p class="card-text">${artist}</p>
@@ -110,6 +93,19 @@ function displaySearchResults(data) {
   $('#searchResults').append(results);
 };
 
+
+const getSelectedTrackInfo = function(trackId) {
+  const url = `https://cors-anywhere.herokuapp.com/https://spotify-scraper.p.rapidapi.com/v1/track/metadata?trackId=${trackId}`;
+  const spotifySearch = {
+    // async: true,
+    // crossDomain: true,
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '5cdbdb46f4mshcde17f8dbd80048p10af3fjsn0b917389a7cd',
+      'X-RapidAPI-Host': 'spotify-scraper.p.rapidapi.com'
+    }
+  };
+}
 
 search.addEventListener('click', function (event) {
   event.preventDefault();
@@ -129,9 +125,9 @@ search.addEventListener('click', function (event) {
 });
 
 // Retrieve Searchs and SearchId from localStorage
-let PreviousSearchs = JSON.parse(localStorage.getItem("user"));
-if (PreviousSearchs === null) {
-  PreviousSearchs = [];
+let previousSeraches = JSON.parse(localStorage.getItem("user"));
+if (previousSeraches === null) {
+  previousSeraches = [];
 }
 
 let SearchId = JSON.parse(localStorage.getItem("SearchId"))
